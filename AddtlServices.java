47a1dded391b112974a2f4fb85763ebe231c6ad9
Rohan Deb Mondal <95281095+rdm-99@users.noreply.github.com>
2022-01-28 21:11:14 +0530

    Part2 Solution Services

---
+++ AddtlServices.java
@@ -0,0 +1,16 @@
+package solution.services;
+
+public class AddtlServices {
+	public void displayPricesAscendingOrder(double[] array) {
+		System.out.println("Stock prices in ascending order are :");
+		for(int i=0;i<array.length;i++)
+			System.out.print(array[i] + " ");
+		System.out.println();
+	}
+	public void displayPricesDescendingOrder(double[] array) {
+		System.out.println("Stock prices in descending order are :");
+		for(int i=array.length-1;i>=0;i--)
+			System.out.print(array[i] + " ");
+		System.out.println();
+	}
+}
